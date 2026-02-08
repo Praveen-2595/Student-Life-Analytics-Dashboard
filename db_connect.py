69d53b5e2d349a537e8d45753c68d0e42e04fc15
praveen-9525 <praveen3coc@gmail.com>
2026-02-08 21:18:02 +0530

    all setup complete

--- db_connect.py
+++ db_connect.py
@@ -1,6 +1,23 @@
-from dotenv import load_dotenv
 import os
+import mysql.connector
+from dotenv import load_dotenv
 
 load_dotenv()
 
-print("DB_NAME from env:", os.getenv("DB_NAME"))
+DB_HOST = os.getenv("DB_HOST")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
+
+conn = mysql.connector.connect(
+    host = DB_HOST,
+    user = DB_USER,
+    password = DB_PASSWORD,
+    database = DB_NAME,
+)
+
+print("connection succesfully")
+
+conn.close()
+
+
